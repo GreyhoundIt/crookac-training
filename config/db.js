f2f69f961b39fb40b588d6584cc6550c5424c66e
Matthew <matthew@greyhoundit.co.uk>
2017-12-28 21:42:34 +0000

    db

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url : 'mongodb://process.env.DB_USER:process.env.DB_PASSWORD@ds133077.mlab.com:33077/crookac'
+    url : "mongodb://'process.env.DB_USER':'process.env.DB_PASSWORD'@ds133077.mlab.com:33077/crookac"
 }
