96635a514362976214e6395ac99ca523d0fb317f
Matthew <matthew@greyhoundit.co.uk>
2017-12-28 22:00:16 +0000

    db

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const db = require('./config/db');
 const app = express();
 
-const port = 8080;
+const port = 5000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
