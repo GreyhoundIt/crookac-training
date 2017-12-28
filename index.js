b6f66aa11fdf7bb39ed662765fd089cd08ff2380
Matthew <matthew@greyhoundit.co.uk>
2017-12-28 22:04:24 +0000

    db

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const db = require('./config/db');
 const app = express();
 
-const port = 5000;
+const PORT = process.env.PORT || 5000
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -17,7 +17,7 @@ MongoClient.connect(db.url, (err, database) => {
     var myDB=database.db('crookac')
     require('./app/routes')(app, myDB);
     var myDB=database.db('crookac')
-    app.listen(port, () => {
-        console.log('We are live on port ' + port);
+    app.listen(PORT, () => {
+        console.log('We are live on port ' + PORT);
     });
 })
