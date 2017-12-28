9fd12417619cc34a61ba6bba1421dd66bcb04094
Matthew <matthew@greyhoundit.co.uk>
2017-12-28 18:09:55 +0000

    beta version

--- app/routes/session_routes.js
+++ app/routes/session_routes.js
@@ -55,4 +55,34 @@ module.exports = function (app, db) {
             }
         });
     });
+
+
+
+    app.get('/next', (req, res) => {
+        let date = require('date-and-time');
+        let now = new Date();
+        now =  date.format(now,'YYYY-MM-DD');
+        const query = { date: { $gte: now} }
+        const orderby = { date: 1 }
+        db.collection('sessions').find(query).sort(orderby).toArray(function(err, item)  {
+            if (err) {
+                res.send({'error': 'A error has occured'});
+            } else {
+                res.send(item[0]);
+            }
+        });
+    });
+
+    app.get('/date/:date', (req, res) => {
+        let dateinput = req.params.date;
+        const query = { date: { $gte: dateinput} }
+        const orderby = { date: 1 }
+        db.collection('sessions').find(query).sort(orderby).toArray(function(err, item)  {
+            if (err) {
+                res.send({'error': 'A error has occured'});
+            } else {
+                res.send(item[0]);
+            }
+        });
+    });
 };
